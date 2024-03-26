26553ec5450da71a09bdbdc2b834111f1152ee26
coderDruti <druti.bose@gmail.com>
2024-03-26 19:40:53 +0530

    Initial stage (0)

---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <div className="Header">
+      <p className="Quiz">InQuizitive</p>
+      <img src="/quizitive/src/assets/Frame_2.jpg" alt="Logo" />
+      <div className="Group1">
+        <div className="Rectangle1" />
+        <div className="CreateQuiz">Create Quiz</div>
+      </div>
+    </div>
+  );
+}
