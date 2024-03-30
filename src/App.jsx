24d2a3b586e8cfdc35429b37f7e135dc698c1f00
coderDruti <druti.bose@gmail.com>
2024-03-30 10:31:56 +0530

    30.03.2024

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./Header";
 import Footer from "./Footer";
+import MainContent from "./MainContent";
 import "./App.css";
 
 function App() {
   return (
     <>
       <Header />
+      <MainContent />
       <Footer />
     </>
   );
--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -1,3 +1,43 @@
 export default function MainContent() {
-  return <div className="MainContent"></div>;
+  return (
+    <div className="MainContent">
+      <div className="para1">
+        <p>
+          The testing effect (also known as retrieval practice, active recall,
+          practice testing, or test-enhanced learning) suggests long-term memory
+          is increased when part of the learning period is devoted to retrieving
+          information from memory.
+        </p>
+        <img
+          className="ActiveRecall"
+          src="/1_Urodyrzl4AWZdI-nob5b7g.png"
+          alt="Active Recall"
+        />
+      </div>
+      <div className="para1">
+        <img src="/quiz.jpeg" alt="Brain" />
+        <p>
+          Inquizitive, being a quiz app, executes the Testing effect (as
+          aforesaid) by letting educators and students create quizes to
+          strengthen their memory.
+        </p>
+      </div>
+      <p>
+        The quizes can be created on various concepts and topics and of varying
+        difficulty levels to practice desirable difficulty as stated in the
+        following study: According to the retrieval effort hypothesis, difficult
+        but successful retrievals are better for memory than easier successful
+        retrievals which supports the idea of finding a desirable difficulty
+        within the retrieval practice considering our memory biases.
+      </p>
+      <p>
+        Quizing oneself repeatedly (a.k.a spaced repetion) guarantees that the
+        concepts will be embedded in the long-term memory for <i>long</i> indeed
+      </p>
+      <p>
+        So create quizes and be Inquizitive with our amazing software!! <br />
+        <strong>Happy learning!!</strong>
+      </p>
+    </div>
+  );
 }
