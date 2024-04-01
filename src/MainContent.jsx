8df6136586d38adecd0a414d53644df6890bb0b7
coderDruti <druti.bose@gmail.com>
2024-04-01 12:00:41 +0530

    01.04.2024(1)

--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -1,28 +1,31 @@
 export default function MainContent() {
   return (
     <div className="MainContent">
-      <div className="para1">
-        <p>
-          The testing effect (also known as retrieval practice, active recall,
-          practice testing, or test-enhanced learning) suggests long-term memory
-          is increased when part of the learning period is devoted to retrieving
-          information from memory.
+      <div className="para">
+        <p className="para1">
+          The <i>testing effect </i>(also known as{" "}
+          <i>retrieval practice, active recall, practice testing</i>, or{" "}
+          <i>test-enhanced learning</i>){" "}
         </p>
         <img
           className="ActiveRecall"
           src="/1_Urodyrzl4AWZdI-nob5b7g.png"
           alt="Active Recall"
         />
+        <p className="para1">
+          suggests long-term memory is increased when part of the learning
+          period is devoted to retrieving information from memory.
+        </p>
       </div>
-      <div className="para1">
+      <div className="para">
         <img src="/quiz.jpeg" alt="Brain" />
-        <p>
+        <p className="para2">
           Inquizitive, being a quiz app, executes the Testing effect (as
           aforesaid) by letting educators and students create quizes to
           strengthen their memory.
         </p>
       </div>
-      <p>
+      <p className="para3">
         The quizes can be created on various concepts and topics and of varying
         difficulty levels to practice desirable difficulty as stated in the
         following study: According to the retrieval effort hypothesis, difficult
@@ -30,11 +33,11 @@ export default function MainContent() {
         retrievals which supports the idea of finding a desirable difficulty
         within the retrieval practice considering our memory biases.
       </p>
-      <p>
+      <p className="para4">
         Quizing oneself repeatedly (a.k.a spaced repetion) guarantees that the
         concepts will be embedded in the long-term memory for <i>long</i> indeed
       </p>
-      <p>
+      <p className="para5">
         So create quizes and be Inquizitive with our amazing software!! <br />
         <strong>Happy learning!!</strong>
       </p>
